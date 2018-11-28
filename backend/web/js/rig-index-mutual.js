9f36e8033936ac9b2f4f2ad772dc5c7440fa2324
cocostru <dk.pochtamp@gmail.com>
2018-11-29 06:35:10 +0800

    lazy

--- backend/web/js/rig-index-mutual.js
+++ backend/web/js/rig-index-mutual.js
@@ -179,7 +179,7 @@ var optionsMutual = {
 
                 ticks: {
                     userCallback: function(item, index, all) {
-                        if (!(index == 0) && !(index % 4) && !((index + 1) == all.length)) return item;
+                        if (!(index == 0) && !(index % 3) && !((index + 1) == all.length)) return item;
                     },
                     fontSize: 12,
                     // display: false
