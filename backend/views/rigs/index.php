796c6b8994d1971e5e11eb96e7294fb2c8b6e18d
cocostru <dk.pochtamp@gmail.com>
2018-12-11 05:43:08 +0800

    lazy

--- backend/views/rigs/index.php
+++ backend/views/rigs/index.php
@@ -459,8 +459,8 @@ footer {
                     $html = ['<div style="cursor: pointer" class="pull-left click-rig" data-rig="' . $model->id . '">'];
 
                     if ($model->lastJournal) {
-                        $html[] = '<span class="label label-default" style="width:40px; direction: rtl; text-align: right">' . end($exp) . '.</span>';
-                        $html[] = '<span class="label gpu-state label-' . ($model->lastJournal->up ? 'success' : 'danger') . '">' . preg_replace( '/[^0-9]/', '', $model->hostname ) . '</span>';
+                        // $html[] = '<span class="label label-default" style="width:40px; direction: rtl; text-align: right">' . end($exp) . '.</span>';
+                        $html[] = '<span class="label gpu-state label-' . ($model->lastJournal->up ? 'success' : 'danger') . '">' . $model->shelf . '</span>';
                         $html[] = '<span class="label gpu-count label-' . (count(explode(";", $model->lastJournal->rate_details)) < 8 ? 'warning' : 'success') . '">' . count(explode(";", $model->lastJournal->rate_details)) . '</span>';
                         $html[] = '<span class="label gpu-rate  label-' . ($model->lastJournal->totalHashrate < 210 ? 'warning' : ($model->lastJournal->totalHashrate >= 230 ? 'success' : 'warning')) . '">' . $model->lastJournal->totalHashrate . ' MH/s</span>';
                     }
