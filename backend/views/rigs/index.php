4455db3d6d852baa065d66b8716d11cd7e194d50
cocostru <dk.pochtamp@gmail.com>
2018-12-13 16:11:32 +0800

    lazy

--- backend/views/rigs/index.php
+++ backend/views/rigs/index.php
@@ -51,7 +51,7 @@ $this->registerJs('rigFirstHashrate(' . json_encode($modelFirst->dayRate) . ');'
     box-shadow: inset 0 0 0 4px #3a3a3a, inset 0 0 0 8px #3b3b3b;
 }
 #raw-html::-webkit-scrollbar-track:hover {
-    box-shadow: inset 0 0 0 4px #3a3a3a, inset 0 0 0 8px rgba(255,255,255,.06);
+    box-shadow: inset 0 0 0 4px #3a3a3a, inset 0 0 0 8px rgba(255,255,255,.03);
 }
 
 #raw-html::-webkit-scrollbar-thumb {
